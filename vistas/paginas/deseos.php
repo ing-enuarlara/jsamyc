cd0c2d08ae0757ec7d246a2fb3eb5c03d3fc0c59
Enuar Lara <enuar2110@gmail.com>
2023-04-04 00:10:33 -0500

    JSAMYC-5 - Se crea interfaz para pagina de deseos

--- vistas/paginas/deseos.php
+++ vistas/paginas/deseos.php
@@ -6,23 +6,37 @@
                 <li class="first">
                     <div class="product_img"></div>
                     <div class="product_name">Producto</div>
-                    <div class="product_price text-center">Precio</div>
-                    <div class="product_stock text-center"></div>
+                    <div class="product_price centrar-texto">Precio</div>
+                    <div class="product_stock centrar-texto"></div>
                     <div class="product_add"></div>
-                    <div class="product_remove text-right"></div>
+                    <div class="product_remove aling-right"></div>
                 </li>
-                <li class="grid_item product-item wishlistRow" rel="dije-oso-articulado-circones-blancos-ojos-negros-oro-amarillo-3-5-cm-7-5-gr-1">
-                    <div class="product_img"><a href="/products/dije-oso-articulado-circones-blancos-ojos-negros-oro-amarillo-3-5-cm-7-5-gr-1" class="product-grid-image"><img src="//cdn.shopify.com/s/files/1/0616/4945/3207/products/WhatsAppImage2023-03-28at12.17.15_9fcb9f33-90db-4b8a-805d-18aefd0d18e5_small.jpg?v=1680038657" alt=""></a></div>
-                    <div class="product_name"><a href="/products/dije-oso-articulado-circones-blancos-ojos-negros-oro-amarillo-3-5-cm-7-5-gr-1" class="product-title">DIJE OSO ARTICULADO CIRCONES BLANCOS OJOS NEGROS ORO AMARILLO 3,5 Cm 7,5 Gr</a></div>
-                    <div class="product_price text-center">
-                        <p class="product-price__price regular-product"><span class="money">$2,137,500</span></p>
+                <li class="grid_item product-item wishlistRow">
+                    <div class="product_img">
+                        <a href="#" class="product-grid-image">
+                            <img src="https://via.placeholder.com/75x100" alt="">
+                        </a>
                     </div>
-                    <div class="product_stock text-center instock">En stock</div>
-                    <div class="action product_add"><a href="/products/dije-oso-articulado-circones-blancos-ojos-negros-oro-amarillo-3-5-cm-7-5-gr-1" class="btn btn-view">Ver el producto</a></div>
-                    <div class="product_remove text-right"><a class="btn btn-remove-wishlist btn--secondary" title="Eliminar de la lista de deseos"><i class="ad ad-times-r" aria-hidden="true"></i></a></div>
+                    <div class="product_name">
+                        <a href="#" class="product-title">DIJE OSO ARTICULADO CIRCONES BLANCOS OJOS NEGROS ORO AMARILLO 3,5 Cm 7,5 Gr</a>
+                    </div>
+                    <div class="product_price centrar-texto">
+                        <p class="product-price__price regular-product">
+                            <span class="money">$2,137,500</span>
+                        </p>
+                    </div>
+                    <div class="product_stock centrar-texto instock">En stock</div>
+                    <div class="action product_add">
+                        <a href="#" class="btnCard btnCard__primary">Ver el producto</a>
+                    </div>
+                    <div class="product_remove aling-right">
+                        <a class="btnCard btnCard-remove-wishlist btnCard__secondary" title="Eliminar de la lista de deseos"><i class="fa-solid fa-xmark"></i></a>
+                    </div>
                 </li>
-                <li class="wishlist-empty text-center" style="display:none;">Los productos no se agregaron a la lista de deseos.</li>
+                <li class="wishlist-empty centrar-texto">Los productos no se agregaron a la lista de deseos.</li>
             </ul>
         </div>
     </div>
 </div>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
