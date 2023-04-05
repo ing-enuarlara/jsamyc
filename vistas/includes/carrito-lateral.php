ab414e582cd40165110a44a935b2d47dcfe56f0c
Enuar Lara <enuar2110@gmail.com>
2023-04-04 20:32:55 -0500

    MAIN-00 - Se crea archivo independiente para carrito de compra lateral

---
+++ vistas/includes/carrito-lateral.php
@@ -0,0 +1,39 @@
+<div class="cart_drawer" id="cart_drawer">
+    <a href="#" class="close_cart" id="close_cart"><i class="fa-solid fa-xmark"></i></a>
+    <h4>CARRITO DE COMPRAS</h4>
+    <ul class="mini-products-list">
+        <li id="cart-title">Su carrito actualmente esta vacio</li>
+        <li class="grid">
+            <a href="#" class="grid__item grid__item-prod width">
+                <img src="https://via.placeholder.com/100x100" alt="" class="imgFl">
+            </a>
+            <div class="grid__item grid__item-prod product-details">
+                <a href="#" class="remove"><i class="fa-solid fa-xmark"></i></a>
+                <a href="#" class="pName">CADENA SINGAPUR ITALY ORO AMARILLO 0,95 MM 45 Cm 0,84 Gr</a>
+                <div class="vropts"></div>
+                <div class="vropts"></div>
+                <div class="priceRow">
+                    <span class="product-price">$312.480</span>
+                    <div class="qtyField">
+                        <a href="#" class="qtyBtn"><i class="fa-solid fa-minus"></i></a>
+                        <span class="qtyBtn">1</span>
+                        <a href="#" class="qtyBtn"><i class="fa-solid fa-plus"></i></a>
+                    </div>
+                </div>
+            </div>
+        </li>
+    </ul>
+    <ul class="minicartTol">
+        <li class="cart-subtotal-row">
+            <span><b>Subtotal:</b></span>
+            <span class="product-price"><b>$312.480</b></span>
+        </li>
+        <li class="buttonSet">
+            <p class="cart__shipping">Gastos de envío y descuentos calculado al momento de pagar</p>
+            <form action="#" method="post">
+                <input type="submit" name="checkout" id="cartDWcheckout" class="btnCard btnCard__primary" value="Finalizar pedido">
+            </form>
+            <a id="inlinecheckout-cart" href="#" class="btnCard btnCard__secondary">Ver carrito</a>
+        </li>
+    </ul>
+</div>
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -63,46 +63,8 @@
     </div>
     <?php
         include(RUTA_PROYECTO."vistas/includes/menu-lateral.php");
+        include(RUTA_PROYECTO."vistas/includes/carrito-lateral.php");
     ?>
-    <div class="cart_drawer" id="cart_drawer">
-        <a href="#" class="close_cart" id="close_cart"><i class="fa-solid fa-xmark"></i></a>
-        <h4>CARRITO DE COMPRAS</h4>
-        <ul class="mini-products-list">
-            <li id="cart-title">Su carrito actualmente esta vacio</li>
-            <li class="grid">
-                <a href="#" class="grid__item grid__item-prod width">
-                    <img src="https://via.placeholder.com/100x100" alt="" class="imgFl">
-                </a>
-                <div class="grid__item grid__item-prod product-details">
-                    <a href="#" class="remove"><i class="fa-solid fa-xmark"></i></a>
-                    <a href="#" class="pName">CADENA SINGAPUR ITALY ORO AMARILLO 0,95 MM 45 Cm 0,84 Gr</a>
-                    <div class="vropts"></div>
-                    <div class="vropts"></div>
-                    <div class="priceRow">
-                        <span class="product-price">$312.480</span>
-                        <div class="qtyField">
-                            <a href="#" class="qtyBtn"><i class="fa-solid fa-minus"></i></a>
-                            <span class="qtyBtn">1</span>
-                            <a href="#" class="qtyBtn"><i class="fa-solid fa-plus"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </li>
-        </ul>
-        <ul class="minicartTol">
-            <li class="cart-subtotal-row">
-                <span><b>Subtotal:</b></span>
-                <span class="product-price"><b>$312.480</b></span>
-            </li>
-            <li class="buttonSet">
-                <p class="cart__shipping">Gastos de envío y descuentos calculado al momento de pagar</p>
-                <form action="#" method="post">
-                    <input type="submit" name="checkout" id="cartDWcheckout" class="btnCard btnCard__primary" value="Finalizar pedido">
-                </form>
-                <a id="inlinecheckout-cart" href="#" class="btnCard btnCard__secondary">Ver carrito</a>
-            </li>
-        </ul>
-    </div>
     <div id="wa-chat-btn-root" class="wa-chat-btn-fixed wa-splmn-chat-btn-offset wa-chat-btn-base-cta wa-chat-btn-container-size-big wa-chat-btn-theme-cta-new-inverted">
         <svg xmlns="http://www.w3.org/2000/svg"
             class="icon icon-tabler icon-tabler-brand-whatsapp" viewBox="0 0 24 24"
