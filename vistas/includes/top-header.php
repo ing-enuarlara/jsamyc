42febbd8900c91f9cba0ce9b423f61dfeb4cf844
Enuar Lara <enuar2110@gmail.com>
2023-04-04 20:11:21 -0500

    MAIN-00 - Se crea archivo independiente para top header

---
+++ vistas/includes/top-header.php
@@ -0,0 +1,53 @@
+
+<div class="top-header">
+    <div class="info">
+        <div class="conten__info info__hiden aling-left contact">
+            <a href="tel:+57XXXXXXXXXX"><i class="fa-sharp fa-solid fa-phone"></i> +57 XXX XXX XXXX</a>
+            <a href="mailto:prueba-usuario@prueba.com"><i class="fa-sharp fa-solid fa-envelope"></i>prueba-usuario@prueba.com</a>
+        </div>
+        <div class="conten__info centrar-texto envios"><i class="fa-sharp fa-solid fa-truck"></i> ENVÍOS A TODO
+            EL MUNDO</div>
+        <div class="conten__info info__hiden aling-right social">
+            Siguenos:
+            <ul class="list--inline social-icons">
+                <li><a href="#" class="social-icons__link">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            class="icon icon-tabler icon-tabler-brand-facebook" viewBox="0 0 24 24"
+                            stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path
+                                d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                        </svg>
+                    </a></li>
+                <li><a href="#" class="social-icons__link">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            class="icon icon-tabler icon-tabler-brand-instagram" viewBox="0 0 24 24"
+                            stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <rect x="4" y="4" width="16" height="16" rx="4" />
+                            <circle cx="12" cy="12" r="3" />
+                            <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+                        </svg>
+                    </a></li>
+                <li><a href="#" class="social-icons__link">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            class="icon icon-tabler icon-tabler-brand-tiktok" viewBox="0 0 24 24"
+                            stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M9 12a4 4 0 1 0 4 4v-12a5 5 0 0 0 5 5" />
+                        </svg>
+                    </a></li>
+                <li><a href="#" class="social-icons__link">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            class="icon icon-tabler icon-tabler-brand-whatsapp" viewBox="0 0 24 24"
+                            stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+                            <path
+                                d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
+                        </svg>
+                    </a></li>
+            </ul>
+        </div>
+    </div>
+</div>
