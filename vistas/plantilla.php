bfd6b015937c6fe72625fae6fc8a74eab091c181
Enuar Lara <enuar2110@gmail.com>
2023-04-04 20:38:08 -0500

    MAIN-00 - Se crea archivo independiente para botones flotantes

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -65,18 +65,8 @@
         include(RUTA_PROYECTO."vistas/includes/menu-lateral.php");
         include(RUTA_PROYECTO."vistas/includes/carrito-lateral.php");
         include(RUTA_PROYECTO."vistas/includes/buscador-principal.php");
+        include(RUTA_PROYECTO."vistas/includes/botones-flotantes.php");
     ?>
-    <div id="wa-chat-btn-root" class="wa-chat-btn-fixed wa-splmn-chat-btn-offset wa-chat-btn-base-cta wa-chat-btn-container-size-big wa-chat-btn-theme-cta-new-inverted">
-        <svg xmlns="http://www.w3.org/2000/svg"
-            class="icon icon-tabler icon-tabler-brand-whatsapp" viewBox="0 0 24 24"
-            stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
-            <path
-                d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
-        </svg>
-        <div class="wa-chat-button-cta-text">Escríbenos por Whatsapp</div>
-    </div>
 
     <!--*****SCRIPT*****-->
     <script src="<?=RUTA?>dist/js/funciones.js"></script>
