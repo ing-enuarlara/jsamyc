8315a08e16326d861bb02a7ba20c57ce0a746d9d
Enuar Lara <enuar2110@gmail.com>
2023-06-23 00:59:48 -0500

    Se llama a archivos de configPagina

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
     require_once "controladores/plantilla.controlador.php";
+    require_once "controladores/configPagina.controlador.php";
     
     require_once "modelo/plantilla.modelo.php";
+    require_once "modelo/configPagina.modelo.php";
 
     $plantilla= new ControladorPlantilla();
     $plantilla -> ctrTraerPlantilla();
