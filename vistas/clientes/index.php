e9a0337c85f41c31a7f10602c632ead59ba515cb
Enuar Lara <enuar2110@gmail.com>
2023-07-09 03:57:06 -0500

    Mejora por revisar

--- vistas/clientes/index.php
+++ vistas/clientes/index.php
@@ -11,7 +11,7 @@
             <div class="g9gqqf1 _1fragembe _1fragem1j">
                 <div class="_1fragem15 _1fragembm _1fragemaf CdKUe">
                     <div class="_1fragem15 _1fragemaf _4RxsF">
-                        <?php include(RUTA_PROYECTO."vistas/clientes/includes/header.php"); ?>
+                        <?php include(RUTA_PROYECTO . "vistas/clientes/includes/header.php"); ?>
                         <main onclick="ocultar('Portal1')">
                             <div class="_1fragem15 _1fragemaf">
                                 <div class="_1fragem15 _1fragem8j _1fragem92 _1fragem84 _1fragem9j _1fragemaf _16s97g7r _0pK7i" style="--_16s97g7q: 120rem;">
@@ -20,12 +20,87 @@
                                             <div class="_1fragemaf _1fragem1b _1mrl40q3 _1fragem24 _1fragem2l _1fragem0 _1fragem4 _16s97g73 _16s97g75 _16s97g7b _16s97g7d" style="--_16s97g72: 1fr; --_16s97g74: minmax(0, 1fr); --_16s97g7a: minmax(0, 1fr) minmax(auto, max-content); --_16s97g7c: minmax(0, 1fr);">
                                                 <h1 id="accessible-heading" tabindex="-1" class="_1fragemaf _1fragemak n8k95w2">Pedidos</h1>
                                             </div>
-                                            <section class="_1fragem15 _1fragemaf" aria-label="Confirmado">
+                                            <!-- <section class="_1fragem15 _1fragemaf" aria-label="Confirmado">
                                                 <div class="_1ip0g651 _1fragem1b _1fragemaf _1fragem1w _1fragem2d">
                                                     <div class="_1fragem15 _1fragem3k _1fragem3h _1fragem3n _1fragem3e _1fragem48 _1fragem4d _1fragem4n _1fragem4i _1fragem8i _1fragem8z _1fragem81 _1fragem9g _1fragembq _1fragemaf _16s97g72l tDwb9">
                                                         <div class="_1ip0g651 _1fragem1b _1fragemaf _1fragem1o _1fragem25 _1fragem3a">
                                                             <h3 class="_1fragemaf _1fragemai n8k95w3">Aún no tienes ningún pedido</h3>
                                                             <p class="_1x52f9s1 _1fragemaf _1x52f9sl _1fragem1j _1fragemai">Ve a la tienda para realizar un pedido.</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </section> -->
+                                            <section class="_1fragem17 _1fragemah" aria-label="Confirmado">
+                                                <div class="_1fragem17 _1fragemaj _1fragemah">
+                                                    <h2 class="n8k95w1 _1fragemah n8k95w3">
+                                                        <div class="_1fragem19 _1fragemah _1fragem3a">
+                                                            <div class="_5uqybw2 _1fragem19 _1fragem9t _1fragem1v _1fragem2c _1fragem0 _1fragem4 _1fragem3a">Confirmado</div>
+                                                        </div>
+                                                    </h2>
+                                                </div>
+                                                <div class="_1ip0g651 _1fragem1d _1fragemah _1fragem1y _1fragem2f">
+                                                    <div class="_1ip0g651 _1fragem1d _1fragemah _1fragem1y _1fragem2f">
+                                                        <div class="_1fragem17 _1fragemah">
+                                                            <div class="_1ip0g651 _1fragem1d _1fragemah _1fragem1y _1fragem2f">
+                                                                <ul class="pWX+B XU0Jc">
+                                                                    <li class="_1fragem17 _1fragemah">
+                                                                        <article class="voSFY" aria-labelledby="order-#5334">
+                                                                            <div class="_1fragem17 _1fragemaj _1fragemah">
+                                                                                <h3 id="order-#5334" class="n8k95w1 _1fragemah n8k95w4">Pedido #5334</h3>
+                                                                            </div>
+                                                                            <div class="_1fragem17 _1fragemah lxK+F"><a class="s2kwpi1 _1fragemah _1fragemav _1fragemb4 s2kwpi2 _1fragemao" aria-label="Ver el pedido #5334" href="/61649453207/account/orders/5129943744663"></a>
+                                                                                <div class="_1fragem17 _1fragemah c1uYw">
+                                                                                    <div class="_1fragem17 _1fragem3m _1fragem3j _1fragem3p _1fragem3g _1fragem4a _1fragem4f _1fragem4p _1fragem4k _1fragem8k _1fragem91 _1fragem83 _1fragem9i _1fragembs _1fragemah _16s97g72l tDwb9">
+                                                                                        <div class="_1fragem17 _1fragemah x8O3k">
+                                                                                            <div class="_1ip0g651 _1fragem1d _1fragemah _1fragem1y _1fragem2f">
+                                                                                                <div class="_1fragem17 _1fragem49 _1fragem4e _1fragem4o _1fragem4j _1fragem8k _1fragem91 _1fragem83 _1fragem9i _1fragemah P3U2d">
+                                                                                                    <div class="_1fragemah _1fragem1d _1mrl40q2 _1fragem26 _1fragem2n _1fragem0 _1fragem4 _1fragem3a _1fragem3e _16s97g73 _16s97g75 _16s97g7b _16s97g7d" style="--_16s97g72: 1fr; --_16s97g74: minmax(0, 1fr); --_16s97g7a: minmax(auto, max-content) minmax(0, 1fr); --_16s97g7c: minmax(0, 1fr);">
+                                                                                                        <div class="_1fragem19 _1fragem1f _1fragem38 _1fragem8s _1fragem8y _1fragem8b _1fragem9q _1fragemah"><span class="_1fragem36 _1fragem10 _1fragem9s _1fragem9r _1fragem17 a8x1wui a8x1wum"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" class="a8x1wuo _1fragem17 _1fragem36 _1fragem9s _1fragem9r" focusable="false" aria-hidden="true">
+                                                                                                                    <circle cx="7" cy="7" r="5.6"></circle>
+                                                                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.5 5.2 6.914 9.092a.4.4 0 0 1-.617.061L4.5 7.35"></path>
+                                                                                                                </svg></span></div><span class="_19gi7yt0 _19gi7ytc _1fragem1k _19gi7yt1">Confirmado</span>
+                                                                                                        <div></div><span class="_19gi7yt0 _19gi7ytc _1fragem1k">Última actualización: 9 jul</span>
+                                                                                                    </div>
+                                                                                                </div>
+                                                                                                <div class="pz-Vw">
+                                                                                                    <div class="_1fragem17 _1fragemaj _1fragemah">El pedido incluye CADENA MINI ROLON ITALY ORO AMARILLO 0,7 MM 45 Cm 0,9 Gr.</div>
+                                                                                                    <div class="_1fragem1a _1fragemah" aria-hidden="true">
+                                                                                                        <div class="_1fragemah _1fragem1d _1mrl40q2 _1fragem1v _1fragem2c _16s97g73 _16s97g75 _16s97g7b _16s97g7d   _16s97g722" style="--_16s97g72: 1fr; --_16s97g74: 1fr; --_16s97g7a: minmax(0, 1fr) minmax(0, 1fr); --_16s97g7c: minmax(0, 1fr) minmax(0, 1fr);">
+                                                                                                            <div class="_16s97g77 _16s97g79" style="--_16s97g76: span 2; --_16s97g78: span 2;">
+                                                                                                                <div class="_1h3po421 _1h3po423 _1fragemah" style="--_1h3po420: 1;"><img src="https://cdn.shopify.com/s/files/1/0616/4945/3207/files/WhatsAppImage2023-05-16at14.01.19.jpg?v=1684266816" alt="" class="_1h3po424 _1fragem17 _1fragem9s _1fragem49 _1fragem4e _1fragem4o _1fragem4j _1fragem9w"></div>
+                                                                                                            </div>
+                                                                                                        </div>
+                                                                                                    </div>
+                                                                                                </div>
+                                                                                                <div class="_1ip0g651 _1fragem1d _1fragemah _1fragem1q _1fragem27">
+                                                                                                    <div class="_1ip0g651 _1fragem1d _1fragemah _1fragem26 _1fragem2n"><span class="_19gi7yt0 _19gi7yte _1fragem1l _19gi7yt1">1 artículo</span><span class="_19gi7yt0 _19gi7yte _1fragem1l _19gi7yt7">Pedido #5334</span></div>
+                                                                                                    <div class="_1fragem19 _1fragemah _1fragem3a _16s97g7t" style="--_16s97g7s: 3.2rem;">
+                                                                                                        <div class="_5uqybw2 _1fragem19 _1fragem9t _1fragem1q _1fragem27 _1fragem0 _1fragem4 _1fragem3a"><span class="_19gi7yt0 _19gi7yte _1fragem1l _19gi7yt1">372.200,00&nbsp;$</span>
+                                                                                                            <div class="_1fragem17 _1fragem4b _1fragem4g _1fragem4q _1fragem4l _1fragem8f _1fragem8t _1fragem7y _1fragem9d _1fragemah WaEWE H7Itm">
+                                                                                                                <div class="_1fragemah _1fragem1d _1mrl40q3 _1fragem1v _1fragem2c _1fragem0 _1fragem4 _16s97g73 _16s97g75 _16s97g7b _16s97g7d" style="--_16s97g72: 1fr; --_16s97g74: minmax(0, 1fr); --_16s97g7a: minmax(auto, max-content) minmax(0, 1fr); --_16s97g7c: minmax(0, 1fr);"><span class="_1fragem36 _1fragem10 _1fragem9s _1fragem9r a8x1wu5 _1fragem17 a8x1wui a8x1wum"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" class="a8x1wuo _1fragem17 _1fragem36 _1fragem9s _1fragem9r" focusable="false" aria-hidden="true">
+                                                                                                                            <circle cx="7" cy="7" r="5.6"></circle>
+                                                                                                                            <path stroke-linecap="round" d="M7 10.111V7.1a.1.1 0 0 0-.1-.1h-.678"></path>
+                                                                                                                            <circle cx="7" cy="4.2" r="0.4"></circle>
+                                                                                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M7.002 4.198h-.005v.005h.005v-.005Z"></path>
+                                                                                                                        </svg></span><span class="_19gi7yt0 _19gi7ytc _1fragem1k">Pago vencido</span></div>
+                                                                                                            </div>
+                                                                                                        </div>
+                                                                                                    </div>
+                                                                                                </div>
+                                                                                            </div>
+                                                                                            <div class="_1fragemah _1fragem1d _1mrl40q3 _1fragem1q _1fragem27 _16s97g73 _16s97g75 _16s97g7b _16s97g7d" style="--_16s97g72: minmax(0, 1fr); --_16s97g74: minmax(0, 1fr); --_16s97g7a: minmax(0, 1fr); --_16s97g7c: minmax(0, 1fr);">
+                                                                                                <div class="_1fragem17 _1fragemah _1NbGV BiGRW">
+                                                                                                    <div class="_1fragem17 _1fragem49 _1fragem4e _1fragem4o _1fragem4j _1fragembs _1fragemah _16s97g71y"><a class="QT4by z0Y1j EbLsk AEMQi _bS7e j6D1f janiy" aria-label="Volver a comprar para el pedido #5334" href="https://elitejoyeria.com.co/cart/42946966356119:1?attributes%5Bfrom%5D=new-customer-accounts&amp;storefront=true&amp;checkout%5Bshipping_address%5D%5Bfirst_name%5D=Enuar+Dvd&amp;checkout%5Bshipping_address%5D%5Blast_name%5D=Lara+Rjs&amp;checkout%5Bshipping_address%5D%5Baddress1%5D=Carrera+50+%2345-25&amp;checkout%5Bshipping_address%5D%5Baddress2%5D=401&amp;checkout%5Bshipping_address%5D%5Bcity%5D=Bello&amp;checkout%5Bshipping_address%5D%5Bprovince%5D=Antioquia&amp;checkout%5Bshipping_address%5D%5Bcountry%5D=Colombia&amp;checkout%5Bshipping_address%5D%5Bzip%5D=051053"><span class="AjwsM">Volver a comprar</span></a></div>
+                                                                                                </div>
+                                                                                            </div>
+                                                                                        </div>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                        </article>
+                                                                    </li>
+                                                                </ul>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -35,12 +110,13 @@
                                 </div>
                             </div>
                         </main>
-                        <?php include(RUTA_PROYECTO."vistas/clientes/includes/footer.php"); ?>
+                        <?php include(RUTA_PROYECTO . "vistas/clientes/includes/footer.php"); ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <?php include(RUTA_PROYECTO."vistas/clientes/includes/pie.php"); ?>
+    <?php include(RUTA_PROYECTO . "vistas/clientes/includes/pie.php"); ?>
 </body>
+
 </html>
